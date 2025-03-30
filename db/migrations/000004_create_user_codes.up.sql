11e6ddcb51037116f76bd8ac959d603fee838014
mikhail <mihaelvozhzhov@gmail.com>
2025-03-30 21:14:31 +0700

    Обновил таблицы

--- db/migrations/000004_create_user_codes.up.sql
+++ db/migrations/000004_create_user_codes.up.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS user_codes (
-    user_id INTEGER PRIMARY KEY REFERENCES users (id) ON DELETE CASCADE,
+    telegram VARCHAR(63) PRIMARY KEY REFERENCES users (telegram) ON DELETE CASCADE,
     code INTEGER
 );
--- internal/models/user.go
+++ internal/models/user.go
@@ -6,13 +6,14 @@ type User struct {
 	ID               int64  `gorm:"primaryKey"`
 	Firstname        string `gorm:"size:63"`
 	Lastname         string `gorm:"size:63"`
-	Telegram         string `gorm:"unique,size:255"`
+	Telegram         string `gorm:"unique,size:63"`
+	TelegramID       int64  `gorm:"default:null"`
 	Password         string `gorm:"size:255"`
 	RegistrationDate time.Time
 	Verified         bool `gorm:"default:false"`
 }
 
 type UserCode struct {
-	UserId int64 `gorm:"primaryKey"`
-	Code   int
+	Telegram string `gorm:"primaryKey,unique,size:63"`
+	Code     int
 }
